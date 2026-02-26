9e8acbdc79765b26edfbf315e74e163cc67fa947
LaiBaoxin <2773147697@qq.com>
2026-02-26 20:43:39 +0800

    feat(blockcha): 新增实现 CourseMedal 勋章合约及 Votes 快照测试

---
+++ blockchain/test/CourseMedal.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {CourseMedal} from "../src/CourseMedal.sol";
+
+contract CourseMedalTest is Test {
+    CourseMedal public medal;
+    address public teacher = address(1);
+    address public student = address(2);
+
+    function setUp() public {
+        vm.prank(teacher);
+        medal = new CourseMedal();
+    }
+
+    function testMintAndVotes() public {
+        // 老师给学生发勋章
+        vm.prank(teacher);
+        medal.safeMint(student);
+
+        // 检查勋章余额
+        assertEq(medal.balanceOf(student), 1);
+
+        // 检查当前投票权
+        assertEq(medal.getVotes(student), 1);
+        
+        // 测试“历史快照”
+        uint256 currentBlock = block.number;
+        vm.roll(currentBlock + 1); // 向前推一个区块
+        
+        assertEq(medal.getPastVotes(student, currentBlock), 1);
+        console.log("Student past votes checked!");
+    }
+}
