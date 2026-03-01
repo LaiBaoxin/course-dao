c0d0ff69318f74804bb3eb47f3116ac9867424b4
LaiBaoxin <2773147697@qq.com>
2026-03-01 17:48:03 +0800

    feat(app): 新增基于 clickhouse 的封装通用查询逻辑

--- app/common/db/clickhouse.go
+++ app/common/db/clickhouse.go
@@ -4,7 +4,12 @@ import (
 	"context"
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"time"
 )
+
+type DB struct {
+	Conn driver.Conn
+}
 
 // InitClickHouse 初始化ClickHouse数据库连接
 func InitClickHouse(dsn string) (driver.Conn, error) {
@@ -23,4 +28,20 @@ func InitClickHouse(dsn string) (driver.Conn, error) {
 		return nil, err
 	}
 	return conn, nil
+}
+
+// Exec 通用的执行逻辑
+func (db *DB) Exec(query string, args ...any) error {
+	// 设置上下文超市时间 5 秒
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	return db.Conn.Exec(ctx, query, args...)
+}
+
+// Query 通用的查询逻辑
+func (db *DB) Query(dest any, query string, args ...any) error {
+	// 设置上下文超市时间 5 秒
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	return db.Conn.Select(ctx, dest, query, args...)
 }
