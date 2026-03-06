01a2aaea12163179904f772567a1eb45ddbb3b17
LaiBaoxin <2773147697@qq.com>
2026-03-06 13:43:28 +0800

    fix(contract): 将 EVM 版本升级至 cancun 修复 mcopy 指令引起的兼容性错误

--- blockchain/src/CourseMedal.sol
+++ blockchain/src/CourseMedal.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721Votes} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Votes.sol";
@@ -15,20 +15,24 @@ contract CourseMedal is ERC721, EIP712, ERC721Votes, Ownable {
         Ownable(msg.sender) 
     {}
 
-    // 老师为学生铸造勋章。
-    // 在 DAO 逻辑中，铸造时必须同时执行 _delegate，否则学生没有初始投票权。
     function safeMint(address to) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
-        _delegate(to, to); // 💡自动授权自己，开启投票权快照
+        _delegate(to, to);
     }
 
-    // --- 重写冲突函数 ---
-    function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Votes) returns (address) {
+    function _update(address to, uint256 tokenId, address auth) 
+        internal 
+        override(ERC721, ERC721Votes) 
+        returns (address) 
+    {
         return super._update(to, tokenId, auth);
     }
 
-    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Votes) {
+    function _increaseBalance(address account, uint128 value) 
+        internal 
+        override(ERC721, ERC721Votes) 
+    {
         super._increaseBalance(account, value);
     }
 }
